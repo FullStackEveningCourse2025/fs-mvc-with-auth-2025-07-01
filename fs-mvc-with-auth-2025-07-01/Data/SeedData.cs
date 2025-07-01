5bbb43b9334feeb991fdef13a2b1f9fff8692706
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-07-01 19:14:47 +0100

    Add identity and seeding of roles

---
+++ fs-mvc-with-auth-2025-07-01/Data/SeedData.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace fs_mvc_with_auth_2025_07_01.Data
+{
+    public static class SeedData
+    {
+        public static async Task Initialize(IServiceProvider serviceProvider)
+        {
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<ApplicationDbContext>();
+
+                var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+
+                var roles = new string[] { "Patient", "Doctor", "Admin" };
+
+                foreach (var role in roles)
+                {
+
+                    if(! await roleManager.RoleExistsAsync(role))
+                    {
+                        await roleManager.CreateAsync(new IdentityRole(role));
+                    }
+                }
+
+            }
+        }
+    }
+}
