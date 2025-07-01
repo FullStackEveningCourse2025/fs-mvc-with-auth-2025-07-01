938476b0776cacc1f7ed2e34fbcf2f3d4ee9a0ce
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-07-01 18:47:19 +0100

    Add project files.

---
+++ fs-mvc-with-auth-2025-07-01/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace fs_mvc_with_auth_2025_07_01.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ fs-mvc-with-auth-2025-07-01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace fs_mvc_with_auth_2025_07_01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
